cccc103dc8735b20aa9fceb060622e4258fa9447
Yuya Minami <yuya373@me.com>
2018-04-26 23:46:04 +0900

    restore state

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import Store from 'electron-store'
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
-const store = configureStore();
+const persistentStore = new Store();
+const store = configureStore(persistentStore.get("state", undefined));
 
 render(
   <AppContainer>
