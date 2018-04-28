c0857f10859b476f22fc838fe619dbab73178657
Yuya Minami <yuya373@me.com>
2018-04-28 23:56:37 +0900

    add key scroll

--- app/components/File/PdfFile.js
+++ app/components/File/PdfFile.js
@@ -4,22 +4,43 @@ import Grid from 'material-ui/Grid';
 
 class PdfFile extends Component {
   webviewRef = null;
+  eventToScrollBy = (event) => {
+    const {key, keyCode} = event;
+    switch(keyCode) {
+    case 40: // ArrowDown
+    case 74: // J
+      return 100;
+    case 68: // D
+      return window.innerHeight / 3;
 
-  propergateToPdf(event) {
-    if (this.webviewRef) {
-      const dom = ReactDOM.findDOMNode(this.webviewRef)
-      console.log("DOM", dom);
-      const params = {
-        type: "keyUp",
-        keyCode: event.key.replace("Arrow", ""),
-      };
-      console.log("PARAM", params);
-      dom.sendInputEvent(params);
+
+    case 38: // ArrowUp
+    case 75: // K
+      return -100;
+    case 85: // U
+      return -(window.innerHeight / 3);
     }
+  };
+
+  handleKeyUp = (event) => {
+    const dom = ReactDOM.findDOMNode(this.webviewRef)
+    // console.log("EVENT", event);
+    const by = this.eventToScrollBy(event);
+    if (by) {
+      dom.executeJavaScript(`scrollBy(0, ${by})`)
+    }
+  };
+
+  componentDidMount() {
+    this.webviewRef.focus();
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keyup", this.handleKeyUp);
   }
 
   render() {
-
     const {file} = this.props;
     const src = `file://${file.path}`;
 
