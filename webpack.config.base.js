41b5ee1b74fbf6bd7ded09289df8b9f4546cd87b
Yuya Minami <yuya373@me.com>
2018-04-28 21:52:02 +0900

    fix worker-loader option

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -26,6 +26,9 @@ export default {
         exclude: /node_modules/,
         use: {
           loader: 'worker-loader',
+          options: {
+            inline: true,
+          },
         },
       },
     ]
