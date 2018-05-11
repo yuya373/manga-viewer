dcacd05a3d89d1a713ee5c27d164f5dca62d2fdf
Yuya Minami <yuya373@me.com>
2018-05-12 01:31:14 +0900

    Revert "upgrade electron"
    This reverts commit afb544c1ea9fa24722421bfa8d4b0e74372c9c8f.

--- app/main.dev.js
+++ app/main.dev.js
@@ -31,12 +31,12 @@ const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
   const extensions = [
-    installer.REACT_DEVELOPER_TOOLS,
-    installer.REDUX_DEVTOOLS,
+    'REACT_DEVELOPER_TOOLS',
+    'REDUX_DEVTOOLS'
   ];
 
   return Promise
-    .all(extensions.map((tool) => installer.default(tool.id, forceDownload)))
+    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
     .catch(console.log);
 };
 
