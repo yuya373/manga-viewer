5364bb854dc79c0571314d94a76a80b37abcc92f
Yuya Minami <yuya373@me.com>
2018-06-21 21:51:00 +0900

    load thumbnail once

--- app/containers/Favorite.js
+++ app/containers/Favorite.js
@@ -5,14 +5,24 @@ import Favorite from './../components/Favorite.js';
 import F from './../models/file.js';
 import D from './../models/directory.js';
 
+export function findDirectory(state, path) {
+  return state.directory.directories.
+    find((e) => e.path === path);
+}
+
+export function findFile(directory, path) {
+  return directory.files.
+    find((e) => e.path === path);
+}
+
 function mapStateToProps(state) {
   const files = state.favorite.files.reduce((a, e) => {
     const splitted = e.split("/");
-    const parent = D.create(
-      splitted.slice(0, splitted.length - 1).join("/"),
-      {}
-    );
-    const file = F.create(e, {});
+    const directoryPath =
+          splitted.slice(0, splitted.length - 1).join("/");
+    const parent = findDirectory(state, directoryPath) ||
+          D.create(directoryPath, {});
+    const file = findFile(parent, e) || F.create(e, {});
     return a.concat([{...file, parent}]);
   }, []);
 
--- app/containers/Tag.js
+++ app/containers/Tag.js
@@ -3,6 +3,10 @@ import { push } from 'react-router-redux';
 import Tag from './../components/Tag.js';
 import F from './../models/file.js';
 import D from './../models/directory.js';
+import {
+  findDirectory,
+  findFile,
+} from './Favorite.js';
 
 function mapStateToProps(state, { match }) {
   const tag = match.params.tag;
@@ -11,11 +15,11 @@ function mapStateToProps(state, { match }) {
 
   taggedFiles.forEach((path) => {
     const splitted = path.split("/");
-    const parent = D.create(
-      splitted.slice(0, splitted.length - 1).join("/"),
-      {}
-    );
-    const file = F.create(path, {});
+    const directoryPath =
+          splitted.slice(0, splitted.length - 1).join("/")
+    const parent = findDirectory(state, directoryPath) ||
+          D.create(directoryPath, {});
+    const file = findFile(parent, path) || F.create(path, {});
     files.push({
       ...file,
       parent,
