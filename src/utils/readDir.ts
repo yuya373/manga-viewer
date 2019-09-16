2d9912245cc5709c4f9c7412a445c0b449a899bd
Yuya Minami <yuya373@me.com>
2019-09-16 15:38:57 +0900

    Sort entries by mtime desc

--- src/utils/readDir.ts
+++ src/utils/readDir.ts
@@ -1,4 +1,18 @@
-import { readdir, Dirent } from 'fs';
+import { readdir, Dirent, Stats, stat } from 'fs';
+import { join } from 'path';
+
+function getStats(path: string, entry: Dirent): Promise<Stats> {
+  return new Promise((resolve, reject) => {
+    stat(join(path, entry.name), (err, stats) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(stats);
+    });
+  });
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export function readDir(path: string): Promise<Dirent[]> {
@@ -8,8 +22,17 @@ export function readDir(path: string): Promise<Dirent[]> {
         reject(err);
         return;
       }
-
-      resolve(entries);
+      Promise.all(
+        entries.map(async entry => {
+          const stats = await getStats(path, entry);
+          return { entry, stats };
+        })
+      ).then(es => {
+        es.sort((a, b) => {
+          return b.stats.mtimeMs - a.stats.mtimeMs;
+        });
+        resolve(es.map(e => e.entry));
+      });
     });
   });
 }
