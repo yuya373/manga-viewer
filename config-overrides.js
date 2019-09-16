b3bb31260f0a87c794773d090912c6bad9b8421d
Yuya Minami <yuya373@me.com>
2019-09-17 03:05:02 +0900

    Add webpack worker settings

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
-const { override } = require('customize-cra');
+const { override, addWebpackModuleRule } = require('customize-cra');
 
 module.exports = override(
   config => {
     config.node = {};
     config.target = 'electron-renderer';
+    config.output.globalObject = 'this';
+    config.module.rules.unshift({
+      test: /\.worker\.ts$/,
+      use: [
+        { loader: 'worker-loader' },
+      ],
+    });
     return config;
   },
 );
--- typings/custom.d.ts
+++ typings/custom.d.ts
@@ -2,3 +2,10 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+
+declare module 'worker-loader!*' {
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+  export default WebpackWorker;
+}
